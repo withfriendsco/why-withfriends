e2be61a000b50e97789c271739548e3a3a68d181
Andrew C. Smith <andrewchristophersmith@gmail.com>
2020-12-02 20:29:01 -0800

    Get the button sorted

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,23 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Button from "./Button"
 
 const Header = ({ siteTitle }) => (
-  <header className="fixed w-full flex flex-wrap border-b bg-white border-wfGray-300 justify-center">
-    <div className="top-0 py-4">
-      <h1>
+  <header className="fixed w-full flex flex-nowrap border-b bg-white border-wfGray-300 justify-center">
+    <div className="top-0 py-4 w-full max-w-4xl flex flex-grow items-center">
+      <div className="inline-block">
         <Link to="/" className="color-salmon-600 no-underline">
           {siteTitle}
         </Link>
-      </h1>
+      </div>
+      <a className="inline-block justify-self-end justify-end flex-grow" href="https://withfriends.co/action/364/sign_up/modal">
+        <div className="flex justify-end">
+          <Button variant="salmon">
+            Get Started
+          </Button>
+        </div>
+      </a>
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const IndexPage = () => (
     <div className="w-full">
       <article className="max-w-xl prose border-b border-wfGray-300 pb-4 my-4">
         <div className="prose text-4xl">How memberships can help your business</div>
+        <div className="text-wfGray-600 font-sm">Posted on Dec. 25, 2020 by Joe Ahearn</div>
         <p className="lede prose max-w-none">
           CEO and Co-Founder Joe Ahearn lays out a few simple ways that
           memberships can help your business &mdash; no matter what industry
@@ -20,21 +21,18 @@ const IndexPage = () => (
             ...read more
           </Link>
         </p>
-        <div className="text-wfGray-600 font-sm">Posted on Dec. 25, 2020 by Joe Ahearn</div>
       </article>
 
       <article className="max-w-xl prose border-b border-wfGray-300 pb-4 my-4">
         <div className="prose text-2xl">Subscription Boxes = PROFIT</div>
+        <div className="text-wfGray-600 font-sm">Posted on Dec. 25, 2020 by Joe Ahearn</div>
         <p className="prose max-w-none">
           Here's an article about subscription boxes. Read on to hear why
           shipping something to wherever all the time can do some great things
           for your business.
         </p>
-        <div className="text-wfGray-600 font-sm">Posted on Dec. 25, 2020 by Joe Ahearn</div>
       </article>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
