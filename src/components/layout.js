8b32dc62125a79ffc492d95386761e0f98ddc4ee
Andrew C. Smith <andrewchristophersmith@gmail.com>
2020-12-04 18:48:02 -0800

    Flesh out layout footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,16 +26,33 @@ const Layout = ({ children }) => {
   return (
     <div className="font-sans">
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div className="flex flex-wrap justify-center pt-12 px-4">
-        <div className="w-full max-w-4xl">
+      <div className="flex flex-wrap justify-center pt-32">
+        <div className="w-full sm:px-16">
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.com">Gatsby</a>
-          </footer>
         </div>
       </div>
+      <footer className="mt-16 py-16 bg-wfGray-800 text-white flex">
+        <div className="pl-16 justify-self-start flex-1">
+          <p>Why Withfriends?</p>
+          <p>Terms of Service</p>
+          <p>Code of Conduct</p>
+          <p>Our Team</p>
+          <p>Pricing</p>
+          <p>Contact</p>
+        </div>
+        <div className="flex-1 px-16">
+          <p>
+            Withfriends is dedicated to the preservation of community culture,
+            spaces, and small businesses. Get in touch to learn more.
+          </p>
+        </div>
+        <div className="pr-16 min-w-sm flex-1 justify-self-end">
+          <p>
+            Withfriends is dedicated to the preservation of community culture,
+            spaces, and small businesses. Get in touch to learn more.
+          </p>
+        </div>
+      </footer>
     </div>
   )
 }
