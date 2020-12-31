40432d2f43dd2a73b1553106a2cbfd9d80198cb1
Andrew C. Smith <andrewchristophersmith@gmail.com>
2020-12-30 21:30:20 -0800

    log env variables

--- functions/facebook_login/facebook_login.js
+++ functions/facebook_login/facebook_login.js
@@ -125,6 +125,14 @@ const userCreate = async (userInfo) => {
 }
 
 exports.handler = async function(event, context) {
+  console.log(process.env.GATSBY_GRAPHQL_API)
+  console.log(process.env.GATSBY_PUBLIC_JWT)
+  console.log(process.env.GATSBY_FB_APP_ID)
+  console.log(process.env.GATSBY_FB_REDIRECT_URI)
+  console.log(process.env.BASE_URL)
+  console.log(process.env.FB_APP_SECRET)
+  console.log(process.env.ADMIN_JWT)
+
   const code = event.queryStringParameters.code
   const state = event.queryStringParameters.state
 
