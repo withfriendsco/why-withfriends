1ecd48651274e6a651ac183c45ea273ac55c6692
Kunal Gupta <kunal@withfriends.co>
2021-09-05 19:59:27 -0700

    https://linear.app/withfriends/issue/WIT-5173/modal

--- src/helpers/mixpanel.js
+++ src/helpers/mixpanel.js
@@ -1,5 +1,5 @@
 const mixpanel = require("mixpanel-browser")
-const mixpanelKey = process.env.GATSBY_MIXPANEL_KEY
+const mixpanelKey = (process.env.GATSBY_MIXPANEL_KEY || "287044101a1f47c209745cab2afcbb0e")
 mixpanel.init(mixpanelKey)
 
 const becomeAnOrganizer = (clickLocation) => {
